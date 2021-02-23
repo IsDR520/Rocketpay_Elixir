1d490a8e7152c3099e972445922798098689c182
IsDR520 <is_reis_sp@hotmail.com>
2021-02-23 16:22:59 -0300

    validate

--- lib/rocketpay/user.ex
+++ lib/rocketpay/user.ex
@@ -19,6 +19,11 @@ defmodule Rocketpay.User do
     %__MODULE__{}
     |>cast(params, @required_params)
     |>validate_required(@required_params)
+    |>validate_length(:password_hash, min: 6)
+    |>validate_number(:age, greater_than_or_equal_to: 18)
+    |>validate_format(:email, ~r/@/)
+    |>unique_constraint([:email])
+    |>unique_constraint([:nickname])
     
   end
 end
