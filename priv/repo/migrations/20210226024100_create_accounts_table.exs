f1cf6ac4e5091b0eca0e0b92dc6134fbe19ce11c
IsDR520 <is_reis_sp@hotmail.com>
2021-02-26 12:23:10 -0300

    operation

--- priv/repo/migrations/20210226024100_create_accounts_table.exs
+++ priv/repo/migrations/20210226024100_create_accounts_table.exs
@@ -8,6 +8,6 @@ defmodule Rocketpay.Repo.Migrations.CreateAccountsTable do
       
       timestamps()
     end
-    create constraint(:accounts, :balance_must_be_positiv_or_zero, check: "balance >=0")
+    create constraint(:accounts, :balance_must_be_positive_or_zero, check: "balance >=0")
   end
 end
